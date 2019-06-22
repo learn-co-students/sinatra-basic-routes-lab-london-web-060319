e6221f805ef0ba5d13ad1a478ae9f4f2d2da94c0
Louise Norris <louisenorris73@yahoo.co.uk>
2019-06-22 17:16:52 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Louise"
+  end
+
+  get '/hometown' do
+    "My hometown is Reading"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is too difficult to choose"
+  end
+
+
 end
